86aa9de7c5e0693ae3937dd241fd8926e57814d5
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-10-04 08:47:45 -0600

    index, generate
    fixed the questions in index
    created badge function
    need to create the MD function

--- index.js
+++ index.js
@@ -2,57 +2,64 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateREADME = require("./utils/generateREADME");
 
 // TODO: Create an array of questions for user input
-inquirer.prompt([
-  {
-    type: "input",
-    name: "project",
-    message: "What is the title of your project?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Please enter brief description of your project:",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "Please enter installion instructions:",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Please enter usage information:",
-  },
-  {
-    type: "input",
-    name: "contribution",
-    message: "Please enter contribution instructions:",
-  },
-  {
-    type: "input",
-    name: "test",
-    message: "Please enter testing instructions:",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Which license would you like to use for your application?",
-    choices: ["MIT License", "Apache 2.0 License", "unlicense"],
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Input Email for Submitting Questions or Comments:",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "project",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please enter brief description of your project:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please enter installion instructions:",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please enter usage information:",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "Please enter contribution instructions:",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Please enter testing instructions:",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Which license would you like to use for your application?",
+      choices: ["MIT License", "Apache 2.0 License", "unlicense"],
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Input Email for Submitting Questions or Comments:",
+    },
+  ])
+  .then((answers) => {
+    fs.writeFile("README.md", generateREADME(answers), (err) =>
+      err ? console.log(err) : console.log("Successfully created README.md!")
+    );
+  });
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseBadgeLinks = {
+  MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  Apache:
+    "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  Unlicense:
+    "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
